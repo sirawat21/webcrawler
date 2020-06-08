ce2801725ef33b3a572c4ecda17208e877b4bd84
Sira <sirawat21@gmail.com>
2020-06-08 18:45:52 +1000

    cover pageScraping func with try catch

--- src/helpers/crawler.js
+++ src/helpers/crawler.js
@@ -6,7 +6,7 @@ class crawler {
     constructor(config) {
         this._CONF = { ...config[0] }
         this.store = {
-            pages: [],
+            pages: [], // store the links from urlCrawling func
             result: []
         }
     }
@@ -17,6 +17,7 @@ class crawler {
         return this.store.result
     }
 
+/* Extract the URL from first page and make the lists for further scraping */
     urlCrawling = async () => {
         const nightmare = Nightmare()
         try{
@@ -36,31 +37,36 @@ class crawler {
         }
     }
 
+/* Start scraping webpage from the URL lists */
     pageScraping = async () => {
-        const cache = []
-        for(let page of this.store.pages) {
-            console.log(`${colors.green("[EXTRACT]")}:${colors.yellow(this._CONF.name)} - ${page}`)
-            const nightmare = Nightmare()
-            await nightmare
-                .goto(page)
-                .wait('body')
-                .evaluate((selector) => {
-                    return {
-                        header: document.querySelector(selector.header).innerText,
-                        date: document.querySelector(selector.date).innerText,
-                        author: document.querySelector(selector.author).innerText,
-                        content: document.querySelector(selector.content).innerText
-                    }
-                }, this._CONF.selectors.article)
-                .end()
-                .then( html => { 
-                    cache.push({
-                        url: page,
-                        body: html
+        try{
+            const cache = []
+            for(let page of this.store.pages) {
+                console.log(`${colors.green("[EXTRACT]")}:${colors.yellow(this._CONF.name)} - ${page}`)
+                const nightmare = Nightmare()
+                await nightmare
+                    .goto(page)
+                    .wait('body')
+                    .evaluate((selector) => {
+                        return {
+                            header: document.querySelector(selector.header).innerText,
+                            date: document.querySelector(selector.date).innerText,
+                            author: document.querySelector(selector.author).innerText,
+                            content: document.querySelector(selector.content).innerText
+                        }
+                    }, this._CONF.selectors.article)
+                    .end()
+                    .then( html => { 
+                        cache.push({
+                            url: page,
+                            body: html
+                        })
                     })
-                })
+            }
+            this.store.result = [...cache]
+        } catch(err) {
+            console.log(colors.red("[FUNC: pageScrping]") + err)
         }
-        this.store.result = [...cache]
     }
 }
 
